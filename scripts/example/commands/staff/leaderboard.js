3e5ae45357165def38a34e70a40eabb46f2948df
defowler2005 <defowler2005@gmail.com>
2024-02-25 22:06:57 +1030

    Leaderboard setup

--- scripts/example/commands/staff/leaderboard.js
+++ scripts/example/commands/staff/leaderboard.js
@@ -8,20 +8,21 @@ const registerInformation = {
     name: 'leaderboard',
     staff: 'management',
     description: 'Set a leaderboard in a dimension',
-    usage: '[  ]',
+    usage: '[ leaderboard My_leaderboard\newln_defowler2OO5:_2000 ]',
     example: [
-        'leaderboard text'
+        'leaderboard [String]'
     ]
 };
 
 Server.command.register(registerInformation, (chatmsg, args) => {
     try {
         const { sender } = chatmsg;
-        const name = sender.getName();
+        //const name = sender.getName();
         if (args[0] === undefined) return sender.tellraw('§¶§c§lUAC ► §c§lYou must provide the leaderboard text.')
         const leaderboardText = args[0].replace(/_/g, " ").replace(/\\newln/g, "\n");
 
         if (sender.hasTag('staffstatus')) {
+            //If args 1, 2, 3 are not unndefined use args if they are undefined default at the current player location.
             const x = args[1] !== undefined ? parseInt(args[1]) : sender.location.x;
             const y = args[2] !== undefined ? parseInt(args[2]) : sender.location.y + 1;
             const z = args[3] !== undefined ? parseInt(args[3]) : sender.location.z;
@@ -29,7 +30,7 @@ Server.command.register(registerInformation, (chatmsg, args) => {
             if (!isNaN(x) && !isNaN(y) && !isNaN(z)) {
                 writeLeaderboard(leaderboardText, x, y, z);
             } else {
-                sender.tellraw(`§¶§c§lUAC ► §c§lInvalid coordinates.`);
+                sender.tellraw(`§¶§c§lUAC ► §c§lYou must provide x, y, z coordinates.`);
             }
             
         } else {
